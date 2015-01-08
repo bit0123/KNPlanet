b76183e270916858119c0c0d89aa6cae22b09273
kaushik roy <kaushik.itdu@gmail.com>
2015-01-09 00:01:06 +0600

    initial project.

---
+++ src/com/knfactory/knplanet/KNPlanet.java
@@ -0,0 +1,15 @@
+package com.knfactory.knplanet;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class KNPlanet extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_knplanet);
+	}
+}
